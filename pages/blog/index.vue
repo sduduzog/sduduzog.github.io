3393f35aabb79923b11b9a931083b13142e60e62
Beautus <gumedesduduzo@gmail.com>
2020-02-16 15:46:04 +0200

    minot fix

--- pages/blog/index.vue
+++ pages/blog/index.vue
@@ -1,7 +1,10 @@
 <template>
   <div>
-    <div @click="tray" v-for="blog in blogPosts" :key="blog.slug">
-      <a :href="blog.slug">{{ blog.slug }}</a>
+    <div v-for="blog in blogPosts" :key="blog.slug">
+      <a
+        :href="blog.slug.startsWith('blog') ? blog.slug : 'blog/' + blog.slug"
+        >{{ blog.slug.startsWith('blog') ? blog.slug : 'blog/' + blog.slug }}</a
+      >
     </div>
   </div>
 </template>
@@ -10,12 +13,6 @@ export default {
   computed: {
     blogPosts() {
       return this.$store.state.blogPosts
-    }
-  },
-  methods: {
-    tray() {
-      // eslint-disable-next-line
-      console.log(this.blogPosts)
     }
   }
 }
