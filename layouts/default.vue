77ee12b363395d57499527157e0a54d10237bb3e
sduduzog <gumedesduduzo@gmail.com>
2020-06-18 22:54:18 +0200

    started to add nice stuff

--- layouts/default.vue
+++ layouts/default.vue
@@ -1,21 +1,51 @@
 <template>
-  <div :class="{ dark: darkMode }" class="layout bg-background min-h-screen">
-    <div
-      class="flex text-lg font-light justify-end p-2 text-primary-color max-w-screen-xl m-auto"
+  <div
+    :class="{ dark: darkMode }"
+    class="min-h-screen grid md:grid-cols-2 bg-background"
+  >
+    <!--div
+      class="flex justify-end p-2 m-auto text-lg font-light text-primary-color max-w-screen-xl"
     >
       <span @click="toggleMode" class="p-2">
         <Sun v-if="darkMode" />
         <Moon v-else />
       </span>
+    </div-->
+    <div class="md:pt-32 md:max-w-sm">
+      <img
+        class="h-32 m-auto mt-12 rounded-lg shadow-lg md:mt-18"
+        src="~/assets/img/sdu.jpg"
+        alt="My face"
+      />
+      <h1
+        class="p-4 mt-6 text-lg font-semibold text-center title text-primary-color"
+      >
+        Beautus S. Gumede
+      </h1>
+      <h2 class="mb-4 text-center text-secondary-color">
+        I write code. Lots and lots of code.
+      </h2>
+      <div class="flex justify-center social">
+        <a id="github" href="https://github.com/sduduzog"
+          ><img src="~/assets/img/github.svg" alt="Github icon"
+        /></a>
+        <a id="twitter" href="https://twitter.com/sduduzog">
+          <img src="~/assets/img/twitter.svg" alt="Twitter icon" />
+        </a>
+        <a id="devto" href="https://dev.to/sduduzog"
+          ><img src="~/assets/img/devto.svg" alt="Dev to icon"
+        /></a>
+      </div>
     </div>
-    <nuxt />
+    <div>
+      <!--nuxt /-->
+      <h1 class="">Nothing to see here... just yet.</h1>
+    </div>
   </div>
 </template>
 <script>
-import Sun from '../components/icons/Sun.vue'
-import Moon from '../components/icons/Moon.vue'
 export default {
-  components: { Sun, Moon },
+  // components: { Sun, Moon },
   data() {
     return { darkMode: false }
   },
