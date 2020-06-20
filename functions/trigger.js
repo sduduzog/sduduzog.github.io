813768a41d60a8a7a9f5b0c9f9bc165c99e8f7e2
sduduzog <gumedesduduzo@gmail.com>
2020-06-20 07:25:43 +0200

    added hook

--- functions/trigger.js
+++ functions/trigger.js
@@ -1,6 +1,43 @@
+const request = require('request')
 exports.handler = function (event, context, callback) {
-  callback(null, {
-    statusCode: 200,
-    body: 'Hello, trigger',
-  })
+  if (event.httpMethod !== 'POST' || !event.body) {
+    return callback(null, {
+      statusCode: 200,
+      body: '',
+    })
+  }
+  try {
+    const { data } = JSON.parse(event.body)
+    if (!data) {
+      return callback(null, {
+        statusCode: 500,
+        body: 'Something funky happened',
+      })
+    }
+    const { type, attributes } = data
+    if (type !== 'webhook_event') {
+      return callback(null, {
+        statusCode: 400,
+        body: 'Bad request',
+      })
+    }
+    const { event_type } = attributes
+    if (event_type !== 'article_updated' && event_type !== 'article_created') {
+      return callback(null, {
+        statusCode: 400,
+        body: 'Bad request',
+      })
+    }
+    request.post(process.env.HOOK_URL, function () {
+      callback(null, {
+        statusCode: 200,
+        body: 'Ok',
+      })
+    })
+  } catch (e) {
+    callback(null, {
+      statusCode: 500,
+      body: 'Error',
+    })
+  }
 }
