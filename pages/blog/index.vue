c154609ac409d5d003312455ccb9c4344ea282aa
sduduzog <gumedesduduzo@gmail.com>
2020-08-18 20:36:57 +0200

    foo

--- pages/blog/index.vue
+++ pages/blog/index.vue
@@ -1,9 +1,26 @@
 <template>
-  <div>
+  <div class="max-w-screen-md space-y-6">
     <h1 class="text-2xl lg:text-4xl font-bold text-accent-color">Blog</h1>
-    <p class="text-gray-700 lg:text-2xl">Stay tuned!</p>
+    <p v-if="posts.length === 0" class="text-gray-700 lg:text-2xl">
+      Stay tuned!
+    </p>
+    <div v-for="post of posts" :key="post.slug">
+      <nuxt-link
+        :to="`blog/${post.slug}`"
+        class="text-xl lg:text-3xl font-semibold"
+        >{{ post.title }}</nuxt-link
+      >
+    </div>
   </div>
 </template>
 <script>
-export default {}
+export default {
+  async asyncData({ $content }) {
+    const posts = await $content("blog").fetch()
+    const isDev = process.env.NODE_ENV === "development"
+    return {
+      posts: isDev ? posts : posts.filter((post) => post.published),
+    }
+  },
+}
 </script>
