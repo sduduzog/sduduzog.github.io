2fa1a96a03854f30673e7e5942ee2c4349f68abd
sduduzog <gumedesduduzo@gmail.com>
2020-08-20 16:49:41 +0200

    font on front page

--- pages/index.vue
+++ pages/index.vue
@@ -1,21 +1,21 @@
 <template>
-  <section class="max-w-screen-md space-y-8">
-    <h1 class="text-xl lg:text-3xl font-bold text-accent-color">
+  <section class="max-w-screen-md space-y-8 font-bold">
+    <h1 class="text-xl lg:text-3xl text-accent-color">
       I write code. Lots and lots of code. To be honest, I also delete most of
       it too, but hey, that's how programming is, I guess.
     </h1>
-    <p class="lg:text-2xl">
+    <p class="lg:text-2xl text-gray-800">
       I'm a software developer at
       <a class="underline" href="https://codeo.co.za/team">Codeo_</a> where I've
       contributed to building and breaking really cool stuff. We eventually fix
       most of it I swear.
     </p>
-    <p class="lg:text-2xl">
+    <p class="lg:text-2xl text-gray-800">
       I completed a BSc Computer Science degree at the University of Zululand.
       Although you don't stop learning, you can't be a student forever, and it
       was great fun being a student.
     </p>
-    <p class="lg:text-2xl">
+    <p class="lg:text-2xl text-gray-800">
       I played cricket in high school. I was in various student societies and
       councils during the course of my degree. Apart from that, I'm a code
       enthusiast. A great fan of open source and it's community. I enjoy reading
