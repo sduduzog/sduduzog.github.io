f16fd9347e2b560986917d9f65993ba803001b81
sduduzog <gumedesduduzo@gmail.com>
2020-09-12 21:18:54 +0200

    styling on blog list

--- pages/blog/index.vue
+++ pages/blog/index.vue
@@ -4,24 +4,32 @@
     <p v-if="posts.length === 0" class="text-gray-700 lg:text-2xl">
       Stay tuned!
     </p>
-    <div v-for="post of posts" :key="post.slug" class="p-1 shadow">
+    <nuxt-link
+      v-for="post of posts"
+      :key="post.slug"
+      :to="`blog/${post.slug}`"
+      class="block p-1 lg:p-4 space-y-1 lg:space-y-4 shadow"
+    >
       <img
         v-if="post.coverImage"
         :src="post.coverImage"
+        class="border rouded-sm lg:rounded-lg"
         alt="Blog cover image"
       />
-      <nuxt-link
+      <h2
         :to="`blog/${post.slug}`"
         class="text-xl lg:text-3xl font-bold text-gray-800"
-        >{{ post.title }}</nuxt-link
       >
-    </div>
+        {{ post.title }}
+      </h2>
+    </nuxt-link>
   </div>
 </template>
 <script>
 export default {
   async asyncData({ $content }) {
     const posts = await $content("blog").sortBy("createdAt", "desc").fetch()
+    console.log(posts)
     const isDev = process.env.NODE_ENV === "development"
     return {
       posts: isDev ? posts : posts.filter((post) => post.published),
