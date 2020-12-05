605bfe66b87a570c9af5d4cf68df664621cce9c6
sduduzog <gumedesduduzo@gmail.com>
2020-12-05 18:33:28 +0200

    simplify generate

--- nuxt.config.js
+++ nuxt.config.js
@@ -58,39 +58,7 @@ export default {
     middleware: 'prepareStories',
   },
   generate: {
-    crawler: false,
-    async routes(callback) {
-      const token = `5i2lrLgiTzCtvr9fKcp5PAtt`;
-      const version = 'published';
-      let cache_version = 0;
-
-      const toIgnore = ['home', 'global', 'blog'];
-
-      // other routes that are not in Storyblok with their slug.
-      const routes = ['/']; // adds / directly
-
-      // Load space and receive latest cache version key to improve performance
-      const space_res = await axios.get(
-        `https://api.storyblok.com/v1/cdn/spaces/me?token=${token}`,
-      );
-      // timestamp of latest publish
-      cache_version = space_res.data.space.version;
-
-      // Call for all Links using the Links API: https://www.storyblok.com/docs/Delivery-Api/Links
-      const res = await axios.get(
-        `https://api.storyblok.com/v1/cdn/links?token=${token}&version=${version}&cv=${cache_version}&per_page=100`,
-      );
-      Object.keys(res.data.links).forEach((key) => {
-        if (
-          !toIgnore.includes(res.data.links[key].slug) ||
-          !res.data.links[key].is_folder
-        ) {
-          routes.push(`/${res.data.links[key].slug}`);
-        }
-      });
-
-      callback(null, routes);
-    },
+    routes: ['/'],
   },
   build: {
     /*
