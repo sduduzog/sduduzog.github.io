fc7eb35a3ea14ee613ea84ab2bcf70c328687872
sduduzog <gumedesduduzo@gmail.com>
2020-12-07 04:10:29 +0200

    published when

--- components/global/blog-item.vue
+++ components/global/blog-item.vue
@@ -6,29 +6,57 @@
     >
       {{ blok.name }}
     </nuxt-link>
-    <span class="block text-sm text-gray-600">
-      Published on {{ publishDate }}
-    </span>
+    <div class="block text-sm text-gray-600">
+      <client-only>
+        <span id="foo" ref="dateSpan">Foooo</span>
+      </client-only>
+      <span v-if="!dynamic"> Published on {{ publishDate }} </span>
+    </div>
   </div>
 </template>
 <script>
-import { parseISO, format } from 'date-fns';
+import { parseISO, format, formatDistanceToNow } from 'date-fns';
 
 export default {
   props: {
     blok: { type: Object, default: undefined },
   },
+  data() {
+    return {
+      dynamic: false,
+    };
+  },
   computed: {
-    publishDate() {
+    date() {
       const date =
         this.blok.first_published_at ||
         this.blok.published_at ||
         this.blok.created_at;
-      return format(parseISO(date), 'd MMM yyyy');
+      return parseISO(date);
     },
-    //   publishedOn() {
-    //     return `Published on ${this.publishDate}`;
-    //   },
+    publishDate() {
+      return format(this.date, 'd MMM yyyy');
+    },
+    publishedOn() {
+      return `Published on ${this.publishDate}`;
+    },
+  },
+  mounted() {
+    this.initClientDate();
+  },
+  methods: {
+    initClientDate(count = 10) {
+      this.$nextTick(() => {
+        if (this.$refs.dateSpan) {
+          this.$refs.dateSpan.innerHTML = `Published ${formatDistanceToNow(
+            this.date,
+          )} ago`;
+          this.dynamic = true;
+        } else if (count > 0) {
+          this.initClientDate(count - 1);
+        }
+      });
+    },
   },
 };
 </script>
