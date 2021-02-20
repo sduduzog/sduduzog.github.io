2c11c6fc76a4ebdddfcbf32da2de0a2e182694b1
beautus <gumedesduduzo@gmail.com>
2021-02-20 17:12:38 +0200

    added project section

---
+++ components/global/project-container.vue
@@ -0,0 +1,20 @@
+<template>
+  <div class="grid md:grid-cols-2 gap-2">
+    <component
+      :is="blok.component"
+      v-for="blok in blok.projects"
+      :key="blok._uid"
+      :blok="blok"
+    />
+  </div>
+</template>
+<script>
+export default {
+  props: {
+    blok: {
+      type: Object,
+      default: undefined,
+    },
+  },
+};
+</script>
---
+++ components/global/project.vue
@@ -0,0 +1,37 @@
+<template>
+  <div class="p-4 border rounded space-y-2">
+    <img v-if="logo" class="h-20 w-20 rounded" :src="logo" alt="Project logo" />
+    <div
+      v-else
+      class="h-20 w-20 bg-fuchsia-50 rounded flex justify-center items-center"
+    >
+      <span class="text-3xl font-bold text-fuchsia-600">{{ abbr }}</span>
+    </div>
+    <h3 class="text-xl font-semibold">{{ blok.name }}</h3>
+    <a href="github" class="text-fuchsia-600"> {{ github }}</a>
+  </div>
+</template>
+<script>
+export default {
+  props: {
+    blok: {
+      type: Object,
+      default: undefined,
+    },
+  },
+  computed: {
+    logo() {
+      return this.blok.logo.url;
+    },
+    github() {
+      return this.blok.github_repo.url;
+    },
+    abbr() {
+      return this.blok.name[0];
+    },
+  },
+  mounted() {
+    console.log(this.blok);
+  },
+};
+</script>
