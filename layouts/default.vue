a0aa46e37215eedf7a1f3fcae9a03639ad2d5b4e
Sduu <gumedesduduzo@gmail.com>
2021-05-15 18:58:11 +0200

    more things

--- layouts/default.vue
+++ layouts/default.vue
@@ -27,44 +27,54 @@
   </div>
 </template>
 <script lang="ts">
-import { defineComponent } from '@nuxtjs/composition-api';
+import {
+  defineComponent,
+  ref,
+  useContext,
+  useStatic,
+} from '@nuxtjs/composition-api';
 
 export default defineComponent({
-  data() {
-    return { story: { content: {} } };
-  },
-  async fetch() {
-    const { error, query, isDev } = this.$nuxt.context;
-    const { cacheVersion } = this.$store.state;
-    let editMode = false;
+  setup() {
+    const slug = ref('global');
+    const { $storyapi, route, store, isDev } = useContext();
+    const { query } = route.value;
+    const story = useStatic(
+      async (slug) => {
+        const { cacheVersion } = store.state;
+        const { _storyblok } = query;
 
-    if (
-      query._storyblok ||
-      isDev ||
-      (typeof window !== 'undefined' &&
-        window.localStorage.getItem('_storyblok_draft_mode'))
-    ) {
-      if (typeof window !== 'undefined') {
-        window.localStorage.setItem('_storyblok_draft_mode', '1');
-        if (window.location === window.parent.location) {
-          window.localStorage.removeItem('_storyblok_draft_mode');
+        let editMode = false;
+
+        if (
+          _storyblok ||
+          isDev ||
+          (typeof window !== 'undefined' &&
+            window.localStorage.getItem('_storyblok_draft_mode'))
+        ) {
+          if (typeof window !== 'undefined') {
+            window.localStorage.setItem('_storyblok_draft_mode', '1');
+            if (window.location === window.parent.location) {
+              window.localStorage.removeItem('_storyblok_draft_mode');
+            }
+          }
+
+          editMode = true;
         }
-      }
 
-      editMode = true;
-    }
+        const version = editMode ? 'draft' : 'published';
+        const storyResponse = await $storyapi.get(`cdn/stories/${slug}`, {
+          version,
+          cv: cacheVersion,
+        });
 
-    const version = editMode ? 'draft' : 'published';
-    try {
-      const response = await this.$storyapi.get(`cdn/stories/global`, {
-        version,
-        cv: cacheVersion,
-      });
-      const { story } = response.data;
-      this.story = story;
-    } catch (err) {
-      error({ statusCode: 404 });
-    }
+        const { story } = storyResponse.data;
+        return story;
+      },
+      slug,
+      'options',
+    );
+    return { story };
   },
 });
 </script>
