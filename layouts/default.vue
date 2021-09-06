0fa3967ed20bc81a3f46809d3571ccade849f692
beautus <gumedesduduzo@gmail.com>
2021-09-06 12:44:20 +0200

    simplify dark mode logic

--- layouts/default.vue
+++ layouts/default.vue
@@ -5,9 +5,9 @@
     <div class="absolute top-0 w-full flex justify-end p-2">
       <div
         class="rounded-full p-2 bg-gray-300 dark:bg-gray-300 dark:bg-opcity-50"
-        @click="toggleTheme"
+        @click="() => toggleDark()"
       >
-        <moon-icon v-if="theme === 'dark'" size="1x" />
+        <moon-icon v-if="isDark" size="1x" />
         <sun-icon v-else size="1x" />
       </div>
     </div>
@@ -17,33 +17,16 @@
   </div>
 </template>
 <script lang="ts">
-import { defineComponent, onMounted, watch } from '@nuxtjs/composition-api';
-import { useMediaQuery, useStorage } from '@vueuse/core';
+import { defineComponent } from '@nuxtjs/composition-api';
+import { useDark, useToggle } from '@vueuse/core';
 import { SunIcon, MoonIcon } from 'vue-feather-icons';
 
 export default defineComponent({
   components: { SunIcon, MoonIcon },
   setup() {
-    const isPreferredDark = useMediaQuery('(prefers-color-scheme: dark)');
-    const defaultThemeState = isPreferredDark.value ? 'dark' : 'light';
-    const theme = useStorage('theme', defaultThemeState);
-
-    onMounted(() => toggleThemeClass(theme.value));
-
-    watch(theme, toggleThemeClass);
-
-    function toggleThemeClass(themeString: string) {
-      if (themeString === 'dark') {
-        document.documentElement.classList.add('dark');
-      } else {
-        document.documentElement.classList.remove('dark');
-      }
-    }
-
-    function toggleTheme() {
-      theme.value = theme.value === 'dark' ? 'light' : 'dark';
-    }
-    return { toggleTheme, theme };
+    const isDark = useDark();
+    const toggleDark = useToggle(isDark);
+    return { toggleDark, isDark };
   },
 });
 </script>
