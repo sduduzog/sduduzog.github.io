894aac8edc4b00ceeb3c9f8217c2cac7ca885f0f
beautus <gumedesduduzo@gmail.com>
2021-09-07 00:24:20 +0200

    attempt not to use the fix

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -92,11 +92,11 @@ export default (): NuxtConfig => ({
           exclude: /(node_modules)/,
         });
       }
-      config.module?.rules.push({
-        test: /\.mjs$/,
-        include: /node_modules/,
-        type: 'javascript/auto',
-      });
+      // config.module?.rules.push({
+      //   test: /\.mjs$/,
+      //   include: /node_modules/,
+      //   type: 'javascript/auto',
+      // });
     },
   },
 });
