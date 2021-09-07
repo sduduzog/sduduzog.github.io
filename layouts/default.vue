d4f2a2515673bbfb7cdd8e7839a62b0e4a7d909e
beautus <gumedesduduzo@gmail.com>
2021-09-07 13:01:44 +0200

    move dark mode toggle

--- layouts/default.vue
+++ layouts/default.vue
@@ -2,14 +2,20 @@
   <div
     class="min-h-screen bg-white dark:bg-gray-900 font-sans flex items-center"
   >
-    <div class="absolute top-0 w-full flex justify-end p-2">
-      <div
-        class="rounded-full p-2 bg-gray-300 dark:bg-gray-300 dark:bg-opcity-50"
-        @click="() => toggleDark()"
-      >
-        <moon-icon v-if="isDark" size="1x" />
-        <sun-icon v-else size="1x" />
-      </div>
+    <div
+      class="
+        absolute
+        top-2
+        right-2
+        rounded-full
+        p-2
+        bg-gray-300
+        dark:bg-gray-300 dark:bg-opcity-50
+      "
+      @click="() => toggleDark()"
+    >
+      <moon-icon v-if="isDark" size="1x" />
+      <sun-icon v-else size="1x" />
     </div>
     <div class="w-full max-w-screen-md mx-auto">
       <nuxt />
