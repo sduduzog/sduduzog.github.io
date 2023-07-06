3fa29d81879910943d34f785903879735fd9306d
Beautus <gumedesduduzo@gmail.com>
2023-07-06 21:29:43 +0200

    add a not found page

---
+++ components/DocumentDrivenNotFound.vue
@@ -0,0 +1,13 @@
+<template>
+  <div class="flex py-20">
+    <div class="grow">
+      <h1 class="text-7xl">404</h1>
+      <h2 class="text-2xl">Not found</h2>
+      <p class="text-lg">Dead end lol</p>
+    </div>
+    <img
+      src="@/assets/img/not_found.jpg"
+      class="h-44 w-44"
+      alt="Me dissapointed" />
+  </div>
+</template>
