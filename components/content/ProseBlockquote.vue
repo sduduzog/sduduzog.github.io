35beccc8d3416e5d909a8eb9a3293e03e7330de1
sduduzog <gumedesduduzo@gmail.com>
2023-07-22 17:10:03 +0200

    add a quote

---
+++ components/content/ProseBlockquote.vue
@@ -0,0 +1,5 @@
+<template>
+  <blockquote class="border-l-4 pl-2 italic">
+    <slot />
+  </blockquote>
+</template>
--- components/content/home.vue
+++ components/content/home.vue
@@ -1,12 +1,12 @@
 <template>
-  <div class="pt-12 items-center gap-6 flex flex-wrap sm:flex-nowrap sm:justify-between">
+  <div class="py-12 items-center gap-6 flex flex-wrap sm:flex-nowrap sm:justify-between">
     <div class="text-center w-full sm:w-auto sm:order-last">
       <img
         src="@/assets/img/headshot.jpg"
         alt="My face"
         class="inline-block rounded-full h-32 w-32" />
     </div>
-    <div grow>
+    <div class="grow">
       <h1 class="font-bold text-2xl text-gray-800">Beautus Sduduzo Gumede</h1>
       <h2 class="font-semibold text-gray-600 text-xl">
         Software Engineer at
