2c349cd931cd5976f35da05b5c9ad3b459bd23d4
Beautus <gumedesduduzo@gmail.com>
2023-07-24 14:00:29 +0200

    try full static

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -57,7 +57,9 @@ export default defineNuxtConfig({
   app: {
     head: { htmlAttrs: { lang: 'en' } },
   },
-  experimental: {
-    noScripts: true,
+  $production: {
+    routeRules: {
+      '/**': { static: true },
+    },
   },
 })
