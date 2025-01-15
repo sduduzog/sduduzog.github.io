ea99d4367cadca539b5f82edeef2887785f781eb
Sdu <gumedesduduzo@gmail.com>
2025-01-15 17:17:57 +0200

    fluff

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,8 +1,7 @@
 export default defineNuxtConfig({
-  routeRules: {
-    "/": { prerender: true },
-  },
+	routeRules: {
+		"/": { prerender: true },
+	},
 
-  modules: ["@nuxthq/studio", "@nuxt/content"],
+	modules: ["@nuxthq/studio", "@nuxt/content"],
 });
-
